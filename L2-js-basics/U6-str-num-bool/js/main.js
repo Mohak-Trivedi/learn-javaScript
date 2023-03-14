6247737f821ede5bb5e6b3bdf9f876669b0a9001
Mohak-Trivedi <trivedimohak@gmail.com>
2023-03-14 17:45:16 +0530

    learned primitive data-types and declaring variables

---
+++ L2-js-basics/U6-str-num-bool/js/main.js
@@ -0,0 +1,26 @@
+// Create a String and console.log it
+console.log('Hello!')
+
+// Add two strings together.
+console.log('Mohak' + 'Trivedi')
+
+// Create a Number and console.log it
+console.log(37)
+
+// Add 27 and 73.
+console.log(27 + 73)
+
+// Subtract 30 from 50.
+console.log(50 - 30)
+
+// Multiply 5 and 10
+console.log(5 * 10)
+
+// Divide 100 by 10.
+console.log(100 / 10)
+
+// Get the remainder of 500 divided by 3.
+console.log(500 % 3)
+
+// Create a Boolean and console.log it.
+console.log(true)
---
+++ L2-js-basics/U7-declare-variables/js/main.js
@@ -0,0 +1,16 @@
+// Declare a string with const.
+const str = 'hello'
+console.log(str)
+
+/*
+Do these:
+Declare a variable with let. Call this variable sum. Set it to 0.
+Declare two more variables, num1 and num2. Set these to 300 and 50.
+Reassign sum with the sum of num1 and num2.
+*/
+let sum = 0
+const num1 = 300
+const num2 = 50
+sum = num1 + num2
+sum = num1 + num2
+console.log(sum)
