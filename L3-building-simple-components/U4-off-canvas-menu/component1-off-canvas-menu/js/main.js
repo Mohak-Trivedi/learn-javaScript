3c137623e53238f986919aec5dac0b3296863819
Mohak-Trivedi <trivedimohak@gmail.com>
2023-03-23 18:45:18 +0530

    built an off-canvas menu from scratch

--- L3-building-simple-components/U4-off-canvas-menu/component1-off-canvas-menu/js/main.js
+++ L3-building-simple-components/U4-off-canvas-menu/component1-off-canvas-menu/js/main.js
@@ -1,1 +1,6 @@
 // Start writing JavaScript here!
+const btnEle = document.querySelector('button')
+btnEle.addEventListener('click', function() {
+    const bodyEle = document.querySelector('body')
+    bodyEle.classList.toggle('offsite-is-open')
+});
